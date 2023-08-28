fea30ba257d81a00950688e763dfbb752f790989
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-08-28 13:37:47 +0300

    task 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: the node to be checked
+ * Return: 0 or 1.
+*/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	return (node && node->left == NULL && node->right == NULL);
+}
