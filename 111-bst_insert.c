73933602ed9a5edd1a1f787174e8c680d5d01734
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-08-29 22:52:00 +0300

    task 25

---
+++ 111-bst_insert.c
@@ -0,0 +1,44 @@
+#include "binary_trees.h"
+
+/**
+ * bst_insert - goes through a binary tree using postorder traversal
+ * @tree: the address of the root node
+ * @value: the function to apply
+ * Return: the address of the new node
+*/
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	bst_t *it = *tree;
+
+	if (*tree == NULL)
+	{
+		*tree = binary_tree_node(NULL, value);
+		return (*tree);
+	}
+	while (1)
+	{
+		if (value < it->n)
+		{
+			if (it->left)
+				it = it->left;
+			else
+			{
+				it->left = binary_tree_node(it, value);
+				return (it->left);
+			}
+		}
+		else if (value > it->n)
+		{
+			if (it->right)
+				it = it->right;
+			else
+			{
+				it->right = binary_tree_node(it, value);
+				return (it->right);
+			}
+		}
+		else
+			break;
+	}
+	return (NULL);
+}
