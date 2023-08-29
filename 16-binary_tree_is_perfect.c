61b08f9acf9f7ca813b33edb3cb36f39ec9bd171
George Raafat <georgexraafat@gmail.com>
2023-08-29 02:53:35 -0700

    fix task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,30 +11,26 @@ size_t bi_height(const binary_tree_t *tree)
 	size_t right;
 
 	if (tree == NULL)
-		return (-1);
+		return (0);
 	left = tree->left ? 1 + bi_height(tree->left) : 0;
 	right = tree->right ? 1 + bi_height(tree->right) : 0;
 	return (left > right ? left : right);
 }
 
 /**
- * bi_full - checks if a binary tree is full
- * @tree: Pointer to the node to measures the height
- * Return: 0, 1
- */
-int bi_full(const binary_tree_t *tree)
+ * bi_size - measures the size of a binary tree
+ * @tree: the address of the root node
+ * Return: the size of the binary tree
+*/
+size_t bi_size(const binary_tree_t *tree)
 {
-	int left, right;
+	size_t size = 1;
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left && tree->right)
-	{
-		left = bi_full(tree->left);
-		right = bi_full(tree->right);
-		return (left && right);
-	}
-	return (tree->left == NULL && tree->right == NULL);
+	size += bi_size(tree->left);
+	size += bi_size(tree->right);
+	return (size);
 }
 
 /**
@@ -44,7 +40,11 @@ int bi_full(const binary_tree_t *tree)
 */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	return (!(bi_height(tree->left) - bi_height(tree->right)) && bi_full(tree));
+	size_t num = 2;
+	size_t hight = bi_height(tree);
+
+	while (hight--)
+		num *= 2;
+
+	return (num - 1 == bi_size(tree));
 }
