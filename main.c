d1d4141a88094217ae82f79f834ffc5d353eab8a
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-08-31 13:24:41 +0300

    task 34

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "binary_trees.h"
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Size of the array
+ */
+void print_array(const int *array, size_t size)
+{
+	size_t i;
+
+	for (i = 0; i < size; ++i)
+		printf("(%03d)", array[i]);
+	printf("\n");
+}
 
 /**
  * main - Entry point
@@ -8,16 +24,17 @@
  */
 int main(void)
 {
-    avl_t *tree;
-    int array[] = {
-        79, 47, 68, 87, 84, 91, 21, 32, 34, 2,
-        20, 22, 98, 1, 62, 95
-    };
-    size_t n = sizeof(array) / sizeof(array[0]);
+	avl_t *tree;
+	int array[] = {
+		1, 2, 20, 21, 22, 32, 34, 47, 62, 68,
+		79, 84, 87, 91, 95, 98
+	};
+	size_t n = sizeof(array) / sizeof(array[0]);
 
-    tree = array_to_avl(array, n);
-    if (!tree)
-        return (1);
-    binary_tree_print(tree);
-    return (0);
+	tree = sorted_array_to_avl(array, n);
+	if (!tree)
+		return (1);
+	print_array(array, n);
+	binary_tree_print(tree);
+	return (0);
 }
