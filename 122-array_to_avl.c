90314c4a14f1487becdeb8d7fe3af9ae8f1bf5f1
George Raafat <georgexraafat@gmail.com>
2023-08-31 01:27:38 -0700

    task 32

---
+++ 122-array_to_avl.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_avl - builds a AVL tree from an array
+ * @array: pointer to the first element of the array to be converted
+ * @size: number of element in the array
+ * Return: the address of the root node
+*/
+avl_t *array_to_avl(int *array, size_t size)
+{
+	bst_t *root = NULL;
+	size_t i;
+
+	if (!array)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		avl_insert(&root, array[i]);
+	return (root);
+}
